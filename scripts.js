cc35264d021508ae273f09d582269f524dec8462
Jerzy Boksa <jerzyxboksa@gmail.com>
2024-11-03 09:33:45 +0100

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let sidebar = undefined
+    let sidebarVisible = false
+
+    fetch('/sidebar/sidebar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('sidebar').innerHTML = data;
+
+        });
+
+
+    window.onload = function () {
+        setTimeout(function () {
+            contactButton.classList.add("visible");
+        }, 1500);
+    };
+
+    const contactButton = document.getElementById("contact-btn");
+    const popup = document.querySelector(".popup-visible");
+    const closePopup = document.getElementById("closePopup");
+    const submitButton = document.getElementById("submit");
+
+
+    submitButton.addEventListener("click", function (event) {
+        console.log('submited')
+    })
+
+    contactButton.addEventListener("click", function () {
+        popup.style.display = "flex";
+    });
+
+    closePopup.addEventListener("click", function () {
+        popup.style.display = "none";
+    });
+
+    popup.addEventListener("click", function (event) {
+        if (event.target === popup) {
+            popup.style.display = "none";
+        }
+    });
+
+
+});
+
